a0288c8398a32054fba2fc9adef474f6195da7e4
ramadhan.azka <ramadhan.azka@ui.ac.id>
2022-11-29 19:44:36 +0700

    added download response

--- export/views.py
+++ export/views.py
@@ -1,4 +1,53 @@
+import csv
+import xlsxwriter
+import json
 from django.http import HttpResponse
-from django.shortcuts import render
+from authuser.models import Transaksi
 
-# Create your views here.
+
+def download_csv(request):
+    response = HttpResponse(
+        content_type='text/csv',
+        headers={'Content-Disposition': 'attachment; filename="somefilename.csv"'},
+    )
+
+    writer = csv.writer(response)
+    writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])  # Dummy
+    writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
+
+    return response
+
+
+def download_xlsx(response):
+    response = HttpResponse(
+        content_type='text/vnd.ms-excel',
+        headers={'Content-Disposition': 'attachment; filename="somefilename.xlsx"'},
+    )
+
+    workbook = xlsxwriter.Workbook(response)
+    worksheet = workbook.add_worksheet()
+    worksheet.write('A1', 'Some Data')  # Dummy
+    workbook.close()
+
+    return response
+
+
+def download_json(response):
+    data = {  # Dummy
+        "name": "sathiyajith",
+        "rollno": 56,
+        "cgpa": 8.6,
+        "phonenumber": "9976770500"
+    }
+
+    response = HttpResponse(
+        json.dumps(data),
+        content_type='application/json',
+        headers={'Content-Disposition': 'attachment; filename="somefilename.json"'},
+    )
+
+    return response
+
+
+def fetch_data():  # Returns all wallet, budget, and transaction in dictionary format
+    return ""
