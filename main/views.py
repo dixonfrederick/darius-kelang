3d399eb6742a400e63d9fd937cce2d3f4b03ce3e
Dixon Frederick <dixon.frederick@ui.ac.id>
2022-12-04 19:56:16 +0700

    Temporary remove auth

--- main/views.py
+++ main/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from authuser.views import getBalance
 
 
+
+@api_view(['GET'])
+#@permission_classes((IsAuthenticated, ))
 def homePageView(request):
     if request.user.is_authenticated:
         balance = getBalance(request.user.username)
