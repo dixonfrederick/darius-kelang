b8094e27c8206b936329f5c2fde2ad177db7986d
linusabhyasaui <71819921+linusabhyasaui@users.noreply.github.com>
2022-12-04 16:08:54 +0700

    Update urls.py

--- budgetcategory/urls.py
+++ budgetcategory/urls.py
@@ -4,11 +4,11 @@ from .views import *
 
 urlpatterns = [
     path("home/", homePageView, name="home"),
-    path("budgetcategory/", budgetCategory_MainView, name="budgetcategory"),
+    path("budget-category/", budgetCategory_MainView, name="budget-category"),
     path("add-category", addCategory, name="addCategory"),
     path("delete-category", deleteCategory, name="deleteCategory"),
     path("update-budget-list", updateBudgetList, name="updateBudgetList"),
-    path("update-budgetcategory", updateBudgetCategory, name="updateBudgetCategory"),
+    path("update-budget-category", updateBudgetCategory, name="updateBudgetCategory"),
     path("cancel/", cancelOperation, name="cancelOperation"),
     path("view-budget-entry", viewAllBudgetCategory, name="viewAllBudgetCategory"),
     path("success-operation/", successOperation, name="successOperation"),
