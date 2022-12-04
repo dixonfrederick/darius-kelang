97316faabbb9739d17754e71f594dd6e4f421cc2
linusabhyasaui <71819921+linusabhyasaui@users.noreply.github.com>
2022-12-04 16:52:20 +0700

    tambah validasi transaksi dan handle init wallet (#14)
    Co-authored-by: ahsannajmy <ahsannajmy.ramadhan@gmail.com>

--- authuser/urls.py
+++ authuser/urls.py
@@ -3,7 +3,6 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenRefreshView)
 from .views import getBalance
 urlpatterns = [
-    path('test/', getBalance),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
