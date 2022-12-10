a85dc849c14c6ea900ef720b193e260fe18c5846
Linus <linus@wicaksana.org>
2022-12-10 15:21:22 +0700

    added buttons to main

--- budgetentry/urls.py
+++ budgetentry/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path("home/", homePageView, name="home"),
-    path('dashboard', viewDashboard, name="dashboard"),  # Needs UID
+    path('dashboard/', viewDashboard, name="dashboard"),  # Needs UID
     path('entries/<int:type_ID>/', viewEntries, name="list"),  # Needs UID + TypeID
     path('entry/<int:type_ID>/<int:entryID>/detail/', viewEntryDetail, name="detail"),
     path('entry/<int:type_ID>/create/', createEntry, name="create"),
